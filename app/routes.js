67cf468a1c4324b904c4540eae1f1af0bf05c648
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-02-20 12:15:52 -0500

    Updated twilio

--- app/routes.js
+++ app/routes.js
@@ -1,18 +1,7 @@
-var database = require('../config/database'); 
-var Firebase = require("firebase");
-var firebaseRef = new Firebase(database.url);
 
 module.exports = function (app) {
-    app.get('/api/order', function(req, res) {
-        // get orders from firebase  
-    });
-    
-    app.delete('/api/order/:order_id', function (req, res) {
-        console.log(req.params.order_id);
-    });
-    
-    app.get('*', function (req, res) {
-        res.sendfile('./public/index.html');
+    app.post('/api/sendSms/:phone_number', function(req, res) {
+        
     });
 }
 

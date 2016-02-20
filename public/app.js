65ca3e25cbaf54e82091b47c7357e277ab43de60
Jeffrey Xiao <jeffrey.xiao1998@gmail.com>
2016-02-20 10:38:25 -0500

    Got core features done

--- public/app.js
+++ public/app.js
@@ -1,23 +1,62 @@
-var mod = angular.module('snatch-and-go', []);
 
-function mainController ($scope, $http) {
-    $scope.orders = {};
-    $http.get('/api/orders')
-        .success(function (data) {
-            console.log(data);
-            $scope.orders = data;
-        })
-        .error(function (data) {
-            console.log('Error: ' + data);
-        });
-    
-    $scope.deleteOrder = function (id) {
-        $http.delete('/api/todos/' + id)
-            .success(function (data) {
-                $scope.orders = data;
-            })
-            .error(function (data) {
-                console.log('Error: ' + data);
-            });
-    }
+var ref = new Firebase('https://snatch-and-go.firebaseio.com/');
+
+var getValue = function (key) {
+    if (key == null)
+        return false;
+    return key.$value;
 }
+
+var mod = angular.module('snatch-and-go', ['firebase'])
+    .directive('orderDirective', function() {
+    return {
+      restrict: 'E',
+      scope: {
+        path: '&'
+      },
+      controller: function($scope, $firebaseArray) {
+            $scope.orderListRef = ref.child($scope.path());
+            $scope.orderList = $firebaseArray($scope.orderListRef);
+            $scope.numOfChildren = 0;
+            $scope.numOfChecked = 0;
+            $scope.orderList.$watch(function(snapshot) {
+                if (snapshot.event == 'child_added') {
+                    $scope.numOfChildren++;
+                    if (getValue($scope.orderList.$getRecord(snapshot.key)))
+                        $scope.numOfChecked++;
+                } else if (snapshot.event == 'child_removed') {
+                    $scope.numOfChildren--;
+                    if (getValue($scope.orderList.$getRecord(snapshot.key)))
+                        $scope.numOfChecked--;
+                } else {
+                    if (getValue($scope.orderList.$getRecord(snapshot.key)))
+                        $scope.numOfChecked++;
+                    else
+                        $scope.numOfChecked--;
+                    if ($scope.numOfChecked == $scope.numOfChildren) {
+                        console.log("deleted");
+                        $scope.orderListRef.parent().set(null);
+                    }
+                }
+            });
+      },
+      template:
+        '<table class="inner-table">' +
+            '<tr ng-repeat="(item, selected) in orderList">' +
+                '<td>' + 
+                    '<span ng-class="{checked:selected.$value}">{{orderList.$keyAt(item)}}</span>' +
+                    '<input type="checkbox" ' +
+                        'ng-change="orderList.$save(item)" ' +
+                        'ng-model="selected.$value">' +
+                '</td>' +
+            '</tr>' +
+        '</table>',
+      replace: true
+    };
+  });
+
+
+mod.controller("mainController",  function($scope, $http, $firebaseArray) {
+    $scope.table = $firebaseArray(ref);
+});
+
--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var database = require('./config/database'); 
 var express  = require('express');
 var app      = express();                               // create our app w/ express
 var morgan = require('morgan');                         // log requests to the console (express4)
 var bodyParser = require('body-parser');                // pull information from HTML POST (express4)
 var methodOverride = require('method-override');        // simulate DELETE and PUT (express4)
-var Firebase = require("firebase");
-var firebaseRef = new Firebase(database.url);
 
 
 // configuration =================
@@ -18,7 +15,6 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse applica
 app.use(methodOverride());
 
 require('./app/routes.js')(app);
-
 // listen (start app with node server.js) ======================================
 app.listen(8080);
 console.log("App listening on port 8080");
